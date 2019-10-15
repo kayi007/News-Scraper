9217ea4e4742f4d87da02165be85bb175c791e6c
Kathleen Leung <kathleenleungky@gmail.com>
2019-10-15 16:23:19 -0700

    added Article model, home route, changed html title, and added reset.css

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,4 +1,14 @@
+var axios = require("axios");
+var cheerio = require("cheerio");
+
 // Require all models
 var db = require("../models");
 
-module.exports = function(app) {};
+module.exports = function(app) {
+    // initial page view
+    app.get("/", function(req, res){
+        res.render("index");
+    });
+    // Scrape
+
+};
